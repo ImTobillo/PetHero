94351d857fe7b689f16e53ab1c78cb4a53786311
Lautaro <lau_manzo12@hotmail.com>
2022-10-25 20:48:42 -0300

    Agrego archivos para la base de datos

--- Config/Config.php
+++ Config/Config.php
@@ -1,12 +1,17 @@
 <?php namespace Config;
 
-define("ROOT", dirname(__DIR__) . "/");
-//Path to your project's root folder
-define("FRONT_ROOT", "/PetHero/"); // cambiar
-define("VIEWS_PATH", "Views/");
-define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
-define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
-define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
+    define("ROOT", dirname(__DIR__) . "/");
+    //Path to your project's root folder
+    define("FRONT_ROOT", "/PetHero/"); // cambiar
+    define("VIEWS_PATH", "Views/");
+    define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
+    define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+    define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "img/");
+    // Constantes para la base de datos
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "PetHero");   // Nombre de la base de datos
+    define("DB_USER", "root");
+    define("DB_PASS", "");
 
 ?>
 
