948826f8a15a8639b04ddd4d99e51cb8c68f1911
Catalina Liste <listecatalina7@gmail.com>
2022-11-03 13:12:59 -0300

    Update DuenioController.php

--- Controllers/DuenioController.php
+++ Controllers/DuenioController.php
@@ -3,21 +3,15 @@
 namespace Controllers;
 
 use Models\Dueño as Dueño;
-use DAO\DueñoDAO as DueñoDAO;
 
 class DuenioController
 {
-     private $dueñosDAO;
-     
-     function __construct()
-     {
-        $this->dueñosDAO = new DueñoDAO();
-     }
 
      public function ShowViewReservar($id_guardian){
         require_once VIEWS_PATH . 'validarSesion.php';
         require_once(VIEWS_PATH . 'reservar-guardian.php');
      }
+      
  
 }
 
