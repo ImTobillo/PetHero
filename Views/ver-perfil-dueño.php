e6a663beb30fb334f9391de4850e73cdf766cef0
Catalina Liste <listecatalina7@gmail.com>
2022-11-03 17:54:50 -0300

    Update ver-perfil-dueño.php
    solo puede ver el perfil, no editar

--- Views/ver-perfil-dueño.php
+++ Views/ver-perfil-dueño.php
@@ -7,19 +7,15 @@ require_once 'nav.php';
     <link property="stylesheet" href=" <?php echo CSS_PATH . 'ver-perfil.css' ?> " rel="stylesheet">
 
     <main>
-        <div class="contenedor-perfil">
-            <img src="" alt="foto perfil">
-            <button class="button">Editar</button>
-        </div>
 
         <div class="contenedor-datos">
-            <div class="parrafo"><p>Nombre</p></div>
-            <div class="parrafo"><p>Apellido</p></div>
-            <div class="parrafo"><p>Edad</p></div>
-            <div class="parrafo"><p>Email</p></div>
-            <div class="parrafo"><p>DNI</p></div>
-            <div class="parrafo"><p>Telefono</p></div>
+            <div class="parrafo"><p>Nombre: <?php echo $_SESSION['loggedUser']->getNombre();?></p></div>
+            <div class="parrafo"><p>Apellido: <?php echo $_SESSION['loggedUser']->getApellido();?></p></div>
+            <div class="parrafo"><p>Fecha de nacimiento: <?php echo $_SESSION['loggedUser']->getfechaNacimiento();?></p></div>
+            <div class="parrafo"><p>Email: <?php echo $_SESSION['loggedUser']->getEmail();?></p></div>
+            <div class="parrafo"><p>DNI: <?php echo $_SESSION['loggedUser']->getDni();?></p></div>
         </div>
+
     </main>
 
 <?php require_once 'footer.php' ?>
